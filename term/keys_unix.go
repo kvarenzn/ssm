26b9026b46301ff9da39b4c043c5582c3a34a997
kvarenzn <12197126+kvarenzn@users.noreply.github.com>
2025-08-12 22:38:24 +0800

    key.go syscall.* -> unix.*

--- term/keys_unix.go
+++ term/keys_unix.go
@@ -4,18 +4,19 @@ package term
 
 import (
 	"os"
-	"syscall"
 	"time"
+
+	"golang.org/x/sys/unix"
 )
 
 func readByteWithTimeout(reader *os.File, timeout time.Duration) error {
 	fd := int(reader.Fd())
 
-	var readFds syscall.FdSet
+	var readFds unix.FdSet
 	readFds.Bits[fd/64] |= 1 << (uint(fd) % 64)
 
-	tv := syscall.NsecToTimeval(timeout.Nanoseconds())
-	n, err := syscall.Select(fd+1, &readFds, nil, nil, &tv)
+	tv := unix.NsecToTimeval(timeout.Nanoseconds())
+	n, err := unix.Select(fd+1, &readFds, nil, nil, &tv)
 	if err != nil {
 		return err
 	}
